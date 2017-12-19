5835ff8903d415d1206e6d2a7774dcb1a6bf5aec
Christian Breitkreutz <cbreitkreutz@epages.com>
2017-12-19 23:25:53 +0100

    logarithmic sweep (ugly code)

--- sweep_20Hz_22050Hz_20sek.m
+++ sweep_20Hz_22050Hz_20sek.m
@@ -3,21 +3,34 @@ bitdepth = 16;
 sampleRate = 44100;
 startfreqency = 20;
 endfreqency = sampleRate / 2;
-lenghtInSek = 20;
-
+lenghtInSek = 10;
+figure(1, "position",get(0,"screensize") / 2) # maxi window plot output
 amountOfSamples = sampleRate * lenghtInSek
-
-stepSize = 1 / sampleRate
-frequencePerSample = ((endfreqency - startfreqency) / amountOfSamples) / 2
+stepSize = 1 / sampleRate;
+allOverFrequency = (endfreqency - startfreqency);
+frequencePerSample = (allOverFrequency) / amountOfSamples;
 xRange = 1:amountOfSamples;
 
-timeRange = xRange .* stepSize;
-frequencePerSampleRange =  xRange .* frequencePerSample;
+subplot(2,2,1);
+frequencePerSampleRange = (xRange .** 3) ./ (amountOfSamples ** 3) * endfreqency;
 
-wave = sin(timeRange .* pi .* 2  .* frequencePerSampleRange );
+plot(frequencePerSampleRange);
 
+normalizedTimeRange = (xRange ./ amountOfSamples) ;
+timeRange = normalizedTimeRange * lenghtInSek / 4 ;
+subplot(2,2,2);
+plot(timeRange);
+
+wave = sin(timeRange * pi * 2  .* frequencePerSampleRange );
+
+subplot(2,2,[3 4]);
 plot(wave)
-sound(wave, sampleRate * 2, bitdepth)
+title("sinus sweep");
+xlabel('samples')
+ylabel('amplitude')
+
+sound(wave, sampleRate , bitdepth)
+
 waveTitle = strcat(
     'sinussweep','_',
     num2str(startfreqency),'Hz','_TO_',
